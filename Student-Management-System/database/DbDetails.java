05ca180871293aa0cb347ec3893a86e507259396
himanshu3232 <himanshu2000ranjan@gmail.com>
2024-01-12 20:38:18 +0530

    password hidden

--- Student-Management-System/database/DbDetails.java
+++ Student-Management-System/database/DbDetails.java
@@ -2,6 +2,6 @@ package database;
 
 class DbDetails {
     final static String URL = "jdbc:postgresql://localhost:5432/mydb";
-    final static String USER_NAME = "postgres";
-    final static String PASSWORD = "5844";
+    final static String USER_NAME = "username";
+    final static String PASSWORD = "password";
 }
